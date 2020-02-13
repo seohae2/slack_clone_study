ce160fdb9e15a8bcce435cbd537b8f07ff90063d
seohae <seohae0406@gmail.com>
2020-02-13 12:45:10 +0900

    frontend-2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import styled from "styled-components";
+import ChannelList from "./Components/ChannelList";
 
 function App() {
   return (
-    <>hello</>
+    <MainFrame>
+      <ChannelList></ChannelList>
+    </MainFrame>
   );
 }
+
+const MainFrame = styled.div`
+  display: flex;
+  height: 100%;
+  flex-direction: row;
+`;
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import App from './App';
-import Counter from './Counter';
+import App from './App';
+import GlobalStyle from "./GlobalStyle";
 
 // App 컴포넌트를 'root'라는 ID를 가진 DOM에 씌운다는 의미이다.
 // index.html 에 'root' ID를 가진 DOM 이 있다.
-ReactDOM.render(<Counter />, document.getElementById('root'));
+ReactDOM.render(
+    <>
+    <App />
+    <GlobalStyle></GlobalStyle>
+    </>, 
+    document.getElementById('root')
+);
